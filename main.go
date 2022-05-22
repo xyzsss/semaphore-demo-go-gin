6aef58b64e9a40a66ec7cdc9f9dd76ae9127c846
xyzsss <exuxu@foxmail.com>
2022-05-22 23:30:26 +0800

    list articles

--- main.go
+++ main.go
@@ -12,5 +12,7 @@ func main() {
 	// router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", showIndexPage)
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
 	router.Run()
 }
--- models.article.go
+++ models.article.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type article struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
@@ -14,4 +16,13 @@ var articleList = []article{
 // Return a list of all the articles
 func getAllArticles() []article {
 	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("Article not found")
 }
